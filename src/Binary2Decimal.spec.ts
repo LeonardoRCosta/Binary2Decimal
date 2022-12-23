aa61aad2e012554ef4de5c441d915d69c9b3f161
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-23 10:44:04 -0300

    refactor: improves code visibility

--- src/Binary2Decimal.spec.ts
+++ src/Binary2Decimal.spec.ts
@@ -13,40 +13,49 @@ describe('Binary2Decimal', () => {
 
   it('should return -1 if the string has no comma', () => {
     const binary2decimal = new Binary2Decimal('0110');
+    const binary2decimal2 = new Binary2Decimal('0110101');
 
     expect(binary2decimal.getCommaIndex()).toEqual(-1);
+    expect(binary2decimal2.getCommaIndex()).toEqual(-1);
   });
 
   it('should return true if the string represents a valid binary', () => {
     const binary2decimal = new Binary2Decimal('0110');
+
     const spy = jest
       .spyOn(binary2decimal, 'checkIfIsBinary')
       .mockImplementationOnce(() => true);
+
     expect(binary2decimal.checkIfIsBinary()).toBe(true);
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should return false if the string does not represent a valid binary', () => {
     const binary2decimal = new Binary2Decimal('2345');
+
     const spy = jest
       .spyOn(binary2decimal, 'checkIfIsBinary')
       .mockImplementationOnce(() => false);
+
     expect(binary2decimal.checkIfIsBinary()).toBe(false);
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should return false if the string is filled with commas', () => {
     const binary2decimal = new Binary2Decimal(',,,,,');
+
     expect(binary2decimal.checkIfIsBinary()).toBe(false);
   });
 
   it('should return false if the string starts with a comma', () => {
     const binary2decimal = new Binary2Decimal(',0101');
+
     expect(binary2decimal.checkIfIsBinary()).toBe(false);
   });
 
   it('should throw if the string is empty', () => {
     const binary2decimal = new Binary2Decimal('');
+
     const spy = jest
       .spyOn(binary2decimal, 'validate')
       .mockImplementationOnce(() => {
@@ -54,6 +63,7 @@ describe('Binary2Decimal', () => {
           'Invalid binary! The string must have at least one algorism'
         );
       });
+
     expect(() => binary2decimal.validate()).toThrowError(
       'Invalid binary! The string must have at least one algorism'
     );
@@ -62,26 +72,32 @@ describe('Binary2Decimal', () => {
 
   it('should throw if the string is not a valid binary', () => {
     const binary2decimal = new Binary2Decimal('abc');
+
     const spy = jest
       .spyOn(binary2decimal, 'validate')
       .mockImplementationOnce(() => {
         throw new Error('Invalid binary!');
       });
+
     expect(() => binary2decimal.validate()).toThrowError('Invalid binary!');
     expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should return the binary converted to a decimal number', () => {
     const binary2decimal = new Binary2Decimal('0110');
+
     const spy = jest
       .spyOn(binary2decimal, 'run')
       .mockImplementationOnce(() => 6);
+
     expect(binary2decimal.run()).toBe(6);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should parse and push the algorism into the array, only if it is not a comma', () => {
     const mockAlgorismsArray: number[] = [];
     const exampleBinaryString: string = '0110,002';
+
     const mockPushMethod = jest.fn().mockImplementation(() => {
       for (const char of exampleBinaryString) {
         const isNotComma = char !== ',';
@@ -89,19 +105,25 @@ describe('Binary2Decimal', () => {
       }
       console.log(mockAlgorismsArray);
     });
+
     mockPushMethod();
+
     expect(mockAlgorismsArray.length).toBe(7);
   });
 
   it('should change the decimalPlaces property to 0 if the binary does not have decimal places', () => {
     const binary2decimal = new Binary2Decimal('0110');
+
     binary2decimal.countDecimalPlaces();
+
     expect(binary2decimal.getDecimalPlaces).toBe(0);
   });
 
   it('should change the decimalPlaces property to the number of the decimal places present in the binary number', () => {
     const binary2decimal = new Binary2Decimal('0110,112');
+
     binary2decimal.countDecimalPlaces();
+
     expect(binary2decimal.getDecimalPlaces).toBe(3);
   });
 });
