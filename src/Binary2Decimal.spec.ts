91d184c32ef029be6556ee220caab2c92c863776
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-23 10:36:34 -0300

    feat: testing method getCommaIndex & checkIfIsBinary

--- src/Binary2Decimal.spec.ts
+++ src/Binary2Decimal.spec.ts
@@ -1,25 +1,20 @@
 import { Binary2Decimal } from './Binary2Decimal';
 
-
 describe('Binary2Decimal', () => {
   afterEach(() => jest.clearAllMocks());
 
-  it('should return true if the string contains a comma', () => {
-    const binary2decimal = new Binary2Decimal('0110,1');
-    const spy = jest
-      .spyOn(binary2decimal, 'checkIfHasComma')
-      .mockImplementationOnce(() => true);
-    expect(binary2decimal.checkIfHasComma()).toBe(true);
-    expect(spy).toHaveBeenCalledTimes(1);
+  it('should return the comma index if the string has comma', () => {
+    const binary2decimal = new Binary2Decimal('01,1');
+    const binary2decimal2 = new Binary2Decimal('011,1');
+
+    expect(binary2decimal.getCommaIndex()).toEqual(2);
+    expect(binary2decimal2.getCommaIndex()).toEqual(3);
   });
 
-  it('should return false if the string does not contain a comma', () => {
-    const binary2decimal = new Binary2Decimal('01100');
-    const spy = jest
-      .spyOn(binary2decimal, 'checkIfHasComma')
-      .mockImplementationOnce(() => false);
-    expect(binary2decimal.checkIfHasComma()).toBe(false);
-    expect(spy).toHaveBeenCalledTimes(1);
+  it('should return -1 if the string has no comma', () => {
+    const binary2decimal = new Binary2Decimal('0110');
+
+    expect(binary2decimal.getCommaIndex()).toEqual(-1);
   });
 
   it('should return true if the string represents a valid binary', () => {
@@ -38,6 +33,16 @@ describe('Binary2Decimal', () => {
       .mockImplementationOnce(() => false);
     expect(binary2decimal.checkIfIsBinary()).toBe(false);
     expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return false if the string is filled with commas', () => {
+    const binary2decimal = new Binary2Decimal(',,,,,');
+    expect(binary2decimal.checkIfIsBinary()).toBe(false);
+  });
+
+  it('should return false if the string starts with a comma', () => {
+    const binary2decimal = new Binary2Decimal(',0101');
+    expect(binary2decimal.checkIfIsBinary()).toBe(false);
   });
 
   it('should throw if the string is empty', () => {
@@ -75,18 +80,18 @@ describe('Binary2Decimal', () => {
   });
 
   it('should parse and push the algorism into the array, only if it is not a comma', () => {
-    const mockAlgorismsArray: number[] = []
-    const exampleBinaryString: string = '0110,002'
+    const mockAlgorismsArray: number[] = [];
+    const exampleBinaryString: string = '0110,002';
     const mockPushMethod = jest.fn().mockImplementation(() => {
       for (const char of exampleBinaryString) {
-        const isNotComma = char !== ','
-        if (isNotComma) mockAlgorismsArray.push(Number(char))
+        const isNotComma = char !== ',';
+        if (isNotComma) mockAlgorismsArray.push(Number(char));
       }
-      console.log(mockAlgorismsArray)
-    })
+      console.log(mockAlgorismsArray);
+    });
     mockPushMethod();
-    expect(mockAlgorismsArray.length).toBe(7)
-  })
+    expect(mockAlgorismsArray.length).toBe(7);
+  });
 
   it('should change the decimalPlaces property to 0 if the binary does not have decimal places', () => {
     const binary2decimal = new Binary2Decimal('0110');
